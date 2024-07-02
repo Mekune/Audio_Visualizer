fb16bfbe0b0fe229f8660d2c367e3852488bbc98
Mekune <nans.diaz30@gmail.com>
2024-07-02 19:49:22 +0200

    BG + shaking

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
 let mic;
-var fft;
-var particles = [];
+let img;
+let fft;
+let particles = [];
 
-// function preload() {
-//   song = loadSound("song4.mp3");
-// }
+function preload() {
+  img = loadImage("BG.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+  imageMode(CENTER);
+
+  // Charger et appliquer un flou à l'image
+  img.filter(BLUR, 12);
+
+  // Initialiser l'analyseur FFT
   fft = new p5.FFT();
+
+  // Récupérer l'état du microphone depuis localStorage
   let micEnabled = JSON.parse(localStorage.getItem("micEnabled"));
   if (micEnabled) {
     mic = new p5.AudioIn();
@@ -26,58 +35,55 @@ function draw() {
 
   translate(width / 2, height / 2);
 
+  // Analyse FFT
   fft.analyze();
-  var amp = fft.getEnergy(20, 200);
+  let amp = fft.getEnergy(20, 200);
 
-  var wave = fft.waveform();
+  // Rotation aléatoire de l'image si l'amplitude est élevée
+  if (amp > 220) {
+    rotate(random(-0.5, 0.5));
+  }
 
-  for (var t = -1; t <= 1; t += 2) {
+  // Affichage de l'image en arrière-plan
+  image(img, 0, 0, width, height);
+
+  // Dessin des formes basées sur le spectre audio
+  let wave = fft.waveform();
+  for (let t = -1; t <= 1; t += 2) {
     beginShape();
-    for (var i = 0; i < 181; i += 0.5) {
-      var index = floor(map(i, 0, 181, 0, wave.length - 1));
-
-      var r = map(wave[index], -1, 1, 150, 350);
-
-      var x = r * sin(i) * t;
-      var y = r * cos(i);
+    for (let i = 0; i < 181; i += 0.5) {
+      let index = floor(map(i, 0, 181, 0, wave.length - 1));
+      let r = map(wave[index], -1, 1, 150, 350);
+      let x = r * sin(i) * t;
+      let y = r * cos(i);
       vertex(x, y);
     }
     endShape();
   }
 
-  var p = new Particle();
+  // Gestion des particules
+  let p = new Particle();
   particles.push(p);
 
-  for (var i = particles.length - 1; i >= 0; i--) {
+  for (let i = particles.length - 1; i >= 0; i--) {
     if (particles[i].edges()) {
       particles.splice(i, 1); // Supprime la particule si elle sort de l'écran
     } else {
-      particles[i].update(amp > 220);
+      particles[i].update(amp > 220); // Condition pour le mouvement des particules
       particles[i].show();
     }
   }
 }
-
-// function mouseClicked() {
-//   if (song.isPlaying()) {
-//     song.pause();
-//     noLoop();
-//   } else {
-//     song.play();
-//     loop();
-//   }
-// }
 
 class Particle {
   constructor() {
     this.pos = p5.Vector.random2D().mult(250);
     this.vel = createVector(0, 0);
     this.acc = this.pos.copy().mult(random(0.0001, 0.00001));
-
     this.w = random(3, 5);
-
     this.color = [random(200, 255), random(200, 255), random(200, 255)];
   }
+
   update(cond) {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
@@ -89,18 +95,16 @@ class Particle {
       this.pos.add(this.vel);
     }
   }
+
   edges() {
-    if (
+    return (
       this.pos.x < -width / 2 ||
       this.pos.x > width / 2 ||
       this.pos.y < -height / 2 ||
       this.pos.y > height / 2
-    ) {
-      return true;
-    } else {
-      return false;
-    }
+    );
   }
+
   show() {
     noStroke();
     fill(this.color);
